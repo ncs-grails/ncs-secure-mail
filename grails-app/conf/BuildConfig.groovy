b91d70dbbb8b27307d7696f58919667fc14ef4b3
Natalya G. Portnov <ngp@umn.edu>
2012-06-11 18:01:55 -0500

    Upgrading to grails 2.0.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -13,13 +13,15 @@ grails.project.dependency.resolution = {
         grailsPlugins()
         grailsHome()
         grailsCentral()
-		grailsRepo "http://svn.cccs.umn.edu/ncs-grails-plugins"
-		
+		//grailsRepo "http://svn.cccs.umn.edu/ncs-grails-plugins"
+
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
         //mavenLocal()
         mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
+        // uncomment these to enable remote dependency resolution from public Maven repositories
+		mavenRepo "http://artifact.ncs.umn.edu/plugins-release"
+		//mavenRepo "http://snapshots.repository.codehaus.org"
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
@@ -31,4 +33,22 @@ grails.project.dependency.resolution = {
 		compile 'org.apache.ant:ant:1.7.1'
 		compile 'org.apache.ant:ant-launcher:1.7.1'
     }
+	plugins {
+
+		build ":tomcat:${grailsVersion}"
+		compile ":hibernate:${grailsVersion}"
+
+		compile ":audit-logging:0.5.4"
+		compile ":code-coverage:1.2.5"
+		compile ":codenarc:0.16.1"
+		compile ":famfamfam:1.0.1"
+		compile ":file-uploader:1.2"
+		compile ":jquery:1.6.1.1"
+		compile ":jquery-ui:1.8.11"
+		compile ":mail:1.0"
+		compile ":ncs-web-template:0.2"
+		compile ":quartz:0.4.2"
+		compile ":spring-security-core:1.2.7.2"
+		compile ":spring-security-ui:0.1.2"
+	}
 }
